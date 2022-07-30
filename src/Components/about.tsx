13450254941b4348071fbfec0cfb13ad51257c39
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-30 01:33:08 -0400

    Update about.tsx

--- src/Components/about.tsx
+++ src/Components/about.tsx
@@ -3,7 +3,7 @@ import { Avatar, Heading, VStack } from "@chakra-ui/react";
 export const About = () => {
   return (
     <VStack pb="5">
-      <Avatar src="/probablynothing.png" mx="auto" size="2xl" />
+      <Avatar src="/probablynothing.png" mx="auto" size="full" width="69%" />
       <Heading pb="3">gm</Heading>
     </VStack>
   );
