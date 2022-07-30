0aec857112a066be9297dd362f56a92b1b8ba01e
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-30 12:29:27 -0400

    rm network mistmatch & update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {
   useEditionDrop,
   useClaimNFT,
-  useNetworkMismatch,
   // ThirdwebSDKProvider,
 } from "@thirdweb-dev/react";
 import { VStack, Flex, Avatar, Button, Link, Text } from "@chakra-ui/react";
@@ -9,14 +8,13 @@ import { About } from "./Components/about";
 import { Topbuttons } from "./Components/topbuttons";
 // import { useAccount, useEnsName, useSigner } from "wagmi";
 import { useAccount, useEnsName } from "wagmi";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ConnectButton, wallet } from "@rainbow-me/rainbowkit";
 import { useState, useEffect } from "react";
 import { GiSailboat } from "react-icons/gi";
 
 function App() {
   const { address } = useAccount();
   const [walletAddress, setWalletAddress] = useState("");
-  const isMismatched = useNetworkMismatch();
   const { data } = useEnsName({ address: address });
   const editionDrop = useEditionDrop(
     "0xF1cC36db8b8C48cCe1ebb41Ca8050dd0C36c0897"
@@ -56,62 +54,58 @@ function App() {
           <ConnectButton />
           {address ? (
             <>
-              {!isMismatched && (
-                <VStack>
-                  <Button
-                    colorScheme="purple"
-                    disabled={isLoading}
-                    onClick={() =>
-                      claimNft({ to: address, tokenId: 0, quantity: 1 })
-                    }
-                    _hover={{ transform: "scale(1.1)" }}
-                    size="lg"
-                    mt="3"
-                  >
-                    Claim JCS!
-                  </Button>
-                  <Button
-                    colorScheme="blue"
-                    rightIcon={<GiSailboat />}
-                    onClick={() =>
-                      window.open(
-                        "https://testnets.opensea.io/assets/rinkeby/0xf1cc36db8b8c48cce1ebb41ca8050dd0c36c0897/0",
-                        "_blank"
-                      )
-                    }
-                  >
-                    View on OpenSea
-                  </Button>
-                  <br />
-                  <Button
-                    colorScheme="purple"
-                    disabled={isLoading}
-                    onClick={() =>
-                      claimNft({ to: address, tokenId: 1, quantity: 1 })
-                    }
-                    _hover={{ transform: "scale(1.1)" }}
-                    size="lg"
-                  >
-                    Claim Probably Nothing!
-                  </Button>
-                  <Button
-                    colorScheme="blue"
-                    rightIcon={<GiSailboat />}
-                    onClick={() =>
-                      window.open(
-                        "https://testnets.opensea.io/assets/rinkeby/0xf1cc36db8b8c48cce1ebb41ca8050dd0c36c0897/1",
-                        "_blank"
-                      )
-                    }
-                  >
-                    View on OpenSea
-                  </Button>
-                </VStack>
-              )}
+              <VStack>
+                <Button
+                  colorScheme="purple"
+                  disabled={isLoading}
+                  onClick={() =>
+                    claimNft({ to: address, tokenId: 0, quantity: 1 })
+                  }
+                  _hover={{ transform: "scale(1.1)" }}
+                  size="lg"
+                  mt="3"
+                >
+                  Claim JCS!
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  rightIcon={<GiSailboat />}
+                  onClick={() =>
+                    window.open(
+                      "https://testnets.opensea.io/assets/rinkeby/0xf1cc36db8b8c48cce1ebb41ca8050dd0c36c0897/0",
+                      "_blank"
+                    )
+                  }
+                >
+                  View on OpenSea
+                </Button>
+                <br />
+                <Button
+                  colorScheme="purple"
+                  disabled={isLoading}
+                  onClick={() =>
+                    claimNft({ to: address, tokenId: 1, quantity: 1 })
+                  }
+                  _hover={{ transform: "scale(1.1)" }}
+                  size="lg"
+                >
+                  Claim Probably Nothing!
+                </Button>
+                <Button
+                  colorScheme="blue"
+                  rightIcon={<GiSailboat />}
+                  onClick={() =>
+                    window.open(
+                      "https://testnets.opensea.io/assets/rinkeby/0xf1cc36db8b8c48cce1ebb41ca8050dd0c36c0897/1",
+                      "_blank"
+                    )
+                  }
+                >
+                  View on OpenSea
+                </Button>
+              </VStack>
             </>
-          ) : (
-            <Text></Text>
-          )}
+          ) : null}
           <Link href="https://joshcs.lol" pt="8" isExternal>
             <Avatar src="/favicon.svg" mx="auto" size="md" />
           </Link>
