33907f99af57b8bcbf1731c0b769885a9a675faf
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-08-01 19:34:02 -0400

    resize

--- src/Components/about.tsx
+++ src/Components/about.tsx
@@ -3,7 +3,7 @@ import { Avatar, Heading, VStack } from "@chakra-ui/react";
 export const About = () => {
   return (
     <VStack pb="5">
-      <Avatar src="/probablynothing.png" mx="auto" size="full" width="69%" />
+      <Avatar src="/probablynothing.png" mx="auto" size="full" width="42%" />
       <Heading pb="3">gm</Heading>
     </VStack>
   );
